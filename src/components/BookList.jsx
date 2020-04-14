49857e819bcaea05bb78291ed660fede9fad61f4
Adam Csigas <adam.csigas@adevinta.com>
2020-04-14 13:17:29 +0200

    Booklist w sf component, class component commented out

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, {/*Component,*/ useContext } from 'react';
 import { themeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
+/* class BookList extends Component {
     static contextType = themeContext;
     render() {
         const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;  
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
+            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
                 <ul>
                     <li style={{ background: theme.ui }}>the way of kings</li>
                     <li style={{ background: theme.ui }}>final empire</li>
                     <li style={{ background: theme.ui }}>yolo the woldar</li>
                 </ul>
             </div>
-          );
+        );
     }
+} */
+
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(themeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+            <ul>
+                <li style={{ background: theme.ui }}>the way of kings</li>
+                <li style={{ background: theme.ui }}>final empire</li>
+                <li style={{ background: theme.ui }}>yolo the woldar</li>
+            </ul>
+        </div>
+    );
 }
- 
+
 export default BookList;

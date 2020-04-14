981abea714abbd65a80b275295506fc705964df4
Adam Csigas <adam.csigas@adevinta.com>
2020-04-14 13:32:34 +0200

    NavBar w sf component,NavBar class component commented out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {/* Component, */ useContext } from 'react';
 import { themeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class NavBar extends Component {
+/* class NavBar extends Component {
     render() {
         return (
             <AuthContext>{(authContext) => (
@@ -29,6 +29,25 @@ class NavBar extends Component {
             </AuthContext>
         );
     }
+} */
+
+const NavBar = () => {
+    const { isLightTheme, light, dark } = useContext(themeContext);
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged in' : 'Logged out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
